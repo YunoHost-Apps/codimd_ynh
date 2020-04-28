c57edceb021c4315066f53861daed52a812025db
ericgaspar <junk.eg@free.fr>
2020-04-28 19:58:45 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql apt-transport-https"
 
-nodejs_version=10
+nodejs_version="10"
 
 #=================================================
 # PERSONAL HELPERS
