af485497154908907230614775cf3ac88517df46
ericgaspar <junk.eg@free.fr>
2020-05-22 10:40:49 +0200

    Revert "move to postgres"
    This reverts commit 3bb697888bc26cea0309c512aaf434890767c1aa.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https"
+pkg_dependencies="apt-transport-https"
 
 nodejs_version="10"
 
