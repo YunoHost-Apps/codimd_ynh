52593df4a0bf0b963105fc8b6fd1534da9f98d4a
ericgaspar <junk.eg@free.fr>
2021-05-08 11:07:25 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql apt-transport-https"
 
-nodejs_version=11
+nodejs_version=12
 
 #=================================================
 # PERSONAL HELPERS
